a737de8ca02c0f4ed27a4f4b62c764ad2191d28b
JeongJunSeo99 <wnstj0810@naver.com>
2024-11-01 17:51:10 +0900

    백준 2292 벌집 문제 메모

---
+++ src/B_2292.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class B_2292 {
+
+    public static void main(String[] args) throws IOException {
+        1 -> 방 1개
+        7(6) -> 방 2개
+        19(12) -> 방 3개
+        37(18) -> 방 4개
+        61(24) -> 방 5개
+    }
+}
