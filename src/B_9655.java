1f52b2d5344af7abd01c0fce8ffb53c10cd8e80a
JeongJunSeo99 <wnstj0810@naver.com>
2024-12-02 15:13:23 +0900

    백준 9655-돌 게임 풀이

--- src/B_9655.java
+++ src/B_9655.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class B_9655 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if(N % 2 == 1)
+            System.out.println("SK");
+        else
+            System.out.println("CY");
+    }
 }
